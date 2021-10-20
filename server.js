b65a99e0e9893e5b6858d67eb2f6588412cda3c5
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-10-20 18:16:58 -0500

    build database call for candidates table

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // importing express
 const express = require('express');
+// importing the mysql module
+const mysql = require('mysql2');
 // port designation
 const PORT = process.env.PORT || 3001;
 // app expression
@@ -8,8 +10,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// Connect to the database
+const db = mysql.createConnection(
+  {
+      host: 'localhost',
+      //your mySQL username 
+      user: 'root',
+      //your mySQL password
+      password: 'ch3Ls3a1sBLu3!',
+      database: 'election'
+  },
+  console.log('Connected to the election database.')  
+);
 
 
+// returning all data from the candidates table 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 // Default response for any other request (Not Found) always make sure this is the last route
